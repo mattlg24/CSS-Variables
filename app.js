9b61cb7d8710aaee5eca62d99f339e06d2beed7c
Matt Gardner <matthewlgardner@gmail.com>
2017-04-05 12:11:35 -0600

    add css variables and handleUpdate function

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 window.onload = function() {
 
+let inputs = document.querySelectorAll('.controls input')
 
+function handleUpdate() {
+  let suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
 
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
 }
